15dc2553f2272c08b0d43b3f62f2041e454947d8
ulrjiri <ulrjiri@gmail.com>
2022-03-30 11:54:46 +0200

    Array reverse comments added and test improved.

--- interview-questions/Java/src/main/java/javaInterviews/codingTasksSolution/basic/Arrays.java
+++ interview-questions/Java/src/main/java/javaInterviews/codingTasksSolution/basic/Arrays.java
@@ -19,12 +19,14 @@ public class Arrays {
         int[] a = {1, 2, 3, 4};
         int[] b = reverse(a);
         System.out.println(java.util.Arrays.toString(a));
+        // [1, 2, 3, 4] expected
         System.out.println(java.util.Arrays.toString(b));
         // [4, 3, 2, 1] expected
 
         int[] c = {1, 2, 3, 4, 5, 6, 7};
         int[] d = reverse(c);
         System.out.println(java.util.Arrays.toString(c));
+        // [1, 2, 3, 4, 6, 7] expected
         System.out.println(java.util.Arrays.toString(d));
         // [7, 6, 5, 4, 3, 2, 1] expected
     }
--- interview-questions/Java/src/test/java/javaInterviews/codingTasksSolution/basic/ArraysTest.java
+++ interview-questions/Java/src/test/java/javaInterviews/codingTasksSolution/basic/ArraysTest.java
@@ -17,5 +17,10 @@ class ArraysTest {
         assertArrayEquals(reverse(new int[] { 123, 456 }), new int[] { 456, 123 });
         assertArrayEquals(reverse(new int[] { 1, 2, 3, 4 }), new int[] { 4, 3, 2, 1 });
         assertArrayEquals(reverse(new int[] { 1, 2, 3, 4, 5, 6, 7 }), new int[] { 7, 6, 5, 4, 3, 2, 1 });
+
+        int[] a = { 1, 2, 3};
+        int[] b = reverse(a);
+        assertArrayEquals(a, new int[] { 1, 2, 3 });
+        assertArrayEquals(b, new int[] { 3, 2, 1 });
     }
 }
