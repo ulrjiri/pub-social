e782bad6c7b0b515db70d0743cc80cac241e0a88
Jiri <ulrjiri@gmail.com>
2023-07-14 13:02:38 +0200

    Java11 HashCode and Equals with Set sample.

---
+++ howtos/EqualsAndHashcode/src/main/java/com/google/ulrjiri/Main.java
@@ -0,0 +1,83 @@
+/*
+  Sample program to demonstrate bad and good usage of equals and hashcode.
+  Philosophy: One person can be just once in the barber at one moment in time. It cannot appear twice.
+ */
+package com.google.ulrjiri;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Just a helper to make testing easier
+ */
+interface Hairy {
+    void cutHair();
+}
+
+/**
+ * Bad usage
+ * Equal and HashCode will use all non-static members of the class including hair length.
+ * After haircut, the person becomes somebody else.
+ */
+@EqualsAndHashCode
+@ToString
+class TheBad implements Hairy {
+    public int id = 1;
+    public String name = "Bad";
+    public int hairLength = 10;  // it is really a bad idea to include hair length to hashcode and equals
+
+    @Override
+    public void cutHair() {
+        hairLength--;
+    }
+}
+
+/**
+ * Good usage - hair length in excluded from hashcode and equals.
+ * It means that the person will still be the same independent from the length of hair.
+ */
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+class TheGood implements Hairy {
+    @EqualsAndHashCode.Include
+    public int id = 2;
+    @EqualsAndHashCode.Include
+    public String name = "Good";
+    public int hairLength = 20;
+
+    @Override
+    public void cutHair() {
+        hairLength--;
+    }
+}
+
+public class Main {
+
+    public static void haveHaircut(Hairy hairy) {
+        Set<Hairy> barberShop = new HashSet<>();
+
+        System.out.println("Before Haircut" + hairy);
+        System.out.println("  Hashcode: " + hairy.hashCode());
+        System.out.println("  Come to barber shop? " + barberShop.add(hairy));
+        System.out.println("  In the barber shop? " + barberShop.contains(hairy));
+        System.out.println("This is me in the barber shop right now:" + barberShop);
+
+        System.out.println("Have haircut");
+        hairy.cutHair();
+
+        System.out.println("  Hashcode: " + hairy.hashCode());
+        System.out.println("  Still in the barber shop? " + barberShop.contains(hairy));
+        System.out.println("  Come again to he barber shop? " + barberShop.add(hairy));
+        System.out.println("  Still in the barber shop? " + barberShop.contains(hairy));
+        System.out.println("This is me in the barber shop right now:" + barberShop);
+    }
+
+    public static void main(String[] args) {
+        haveHaircut(new TheBad());
+        System.out.println("----");
+        haveHaircut(new TheGood());
+    }
+}
---
+++ howtos/EqualsAndHashcode/src/main/java/com/google/ulrjiri/MainMinimal.java
@@ -0,0 +1,33 @@
+package com.google.ulrjiri;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@EqualsAndHashCode
+@ToString
+class Item {
+    public int itemId = 1;
+    public boolean isSold = false;
+}
+
+public class MainMinimal {
+    public static void main(String[] args) {
+        Set<Item> set = new HashSet<>();
+
+        Item item = new Item();
+        System.out.println("Hashcode of " + item + " is " + item.hashCode());
+        set.add(item);
+        System.out.println("Is " + item + " in set? " + set.contains(item));
+
+        item.isSold = true;
+        System.out.println("Is " + item + " still in set? " + set.contains(item));
+        System.out.println("Hashcode of " + item + " is " + item.hashCode());
+        set.add(item);
+        System.out.println("Is " + item + " now in set? " + set.contains(item));
+
+        System.out.println("What is in the set now: " + set);
+    }
+}
